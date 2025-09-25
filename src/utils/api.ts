100056677783d2daed9f7f81a0af445393e010a2
Juliana Miranda <rosajmirandar@gmail.com>
2025-09-25 09:34:56 -0500

    primer commit

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import { storageManager } from './storage';
 
-const API_BASE_URL = import.meta.env.DEV ? '/api' : 'https://localhost:7117/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 interface ApiConfig extends RequestInit {
   requiresAuth?: boolean;
